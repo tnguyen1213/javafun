3d2543fc1e53e11ba59db4e12d9135c68a832109
tnguyen1213 <tnguyen1213@hotmail.com>
2015-02-21 08:14:07 -0600

    Added GUI and JAR

--- Patrick/OCA_Programmer1/PatricksUniversGUI/Client/src/com/ttnsystems/javafun/PatricksUniverseGUI.java
+++
@@ -1,7 +0,0 @@
-package com.ttnsystems.javafun;
-
-public class PatricksUniverseGUI {
-    public static void main(String[] args) {
-        PatricksUniverseGUI patricksUniverseGUI = new PatricksUniverseGUI();
-    }
-}
